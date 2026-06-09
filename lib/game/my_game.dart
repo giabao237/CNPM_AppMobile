0ae9e72858533f94749fb385fda7b774f52c4406
giabao237 <giabaoschdtx@gmail.com>
2026-06-09 15:40:25 +0700

    animation attack

--- lib/game/my_game.dart
+++ lib/game/my_game.dart
@@ -1,10 +1,55 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+
 import 'player.dart';
 
 class MyGame extends FlameGame {
+  Player? player;
+  JoystickComponent? joystick;
 
   @override
   Future<void> onLoad() async {
-    await add(Player());
+    player = Player();
+    await add(player!);
+
+    joystick = JoystickComponent(
+      knob: CircleComponent(radius: 25),
+      background: CircleComponent(radius: 60),
+    );
+
+    await add(joystick!);
+
+    player!.position = size / 2;
+    joystick!.position = Vector2(120, size.y - 120);
   }
+
+  @override
+  void onGameResize(Vector2 canvasSize) {
+    super.onGameResize(canvasSize);
+
+    if (joystick != null) {
+      joystick!.position = Vector2(
+        120,
+        canvasSize.y - 120,
+      );
+    }
+
+    if (player != null) {
+      player!.position = canvasSize / 2;
+    }
+  }
+
+  @override
+void update(double dt) {
+  super.update(dt);
+
+  if (player != null && joystick != null) {
+    player!.move(
+      joystick!.relativeDelta,
+      dt,
+    );
+
+    player!.autoAttack(dt);
+  }
+}
 }
--- lib/game/player.dart
+++ lib/game/player.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
@@ -11,50 +13,70 @@ enum PlayerState {
 
 class Player extends SpriteAnimationGroupComponent<PlayerState>
     with HasGameRef<FlameGame> {
-
   Player();
+
+  double attackTimer = 0;
+  bool isAttacking = false;
 
   @override
   Future<void> onLoad() async {
     animations = {
-      PlayerState.idle: await _loadAnimation(
-        "player/idle.png",
-        6,
-      ),
-
-      PlayerState.walk: await _loadAnimation(
-        "player/walk.png",
-        6,
-      ),
-
-      PlayerState.attack: await _loadAnimation(
-        "player/attack.png",
-        12,
-      ),
-
-      PlayerState.hurt: await _loadAnimation(
-        "player/hurt.png",
-        4,
-      ),
-
-      PlayerState.death: await _loadAnimation(
-        "player/death.png",
-        4,
-      ),
+      PlayerState.idle: await _loadAnimation("player/idle.png", 6),
+      PlayerState.walk: await _loadAnimation("player/walk.png", 6),
+      PlayerState.attack: await _loadAnimation("player/attack.png", 12),
+      PlayerState.hurt: await _loadAnimation("player/hurt.png", 4),
+      PlayerState.death: await _loadAnimation("player/death.png", 4),
     };
 
     current = PlayerState.idle;
 
     size = Vector2(100, 100);
-
+    anchor = Anchor.center;
     position = Vector2(200, 300);
+  }
+
+  void move(Vector2 direction, double dt) {
+  const speed = 200.0;
+
+  position += direction * speed * dt;
+
+  // Vẫn cho đổi hướng khi đang attack
+  if (direction.x < -0.1) {
+    scale.x = -1;
+  }
+
+  if (direction.x > 0.1) {
+    scale.x = 1;
+  }
+
+  // Nếu đang attack thì không đổi animation sang walk/idle
+  if (isAttacking) return;
+
+  if (direction.length > 0.1) {
+    current = PlayerState.walk;
+  } else {
+    current = PlayerState.idle;
+  }
+}
+// chỉnh tốc độ đánh attacktimer 
+  void autoAttack(double dt) {
+    attackTimer += dt;
+
+    if (attackTimer >= 2 && !isAttacking) {
+      attackTimer = 0;
+      isAttacking = true;
+      current = PlayerState.attack;
+
+      Future.delayed(const Duration(milliseconds: 1200), () {
+        isAttacking = false;
+      });
+    }
   }
 
   Future<SpriteAnimation> _loadAnimation(
     String path,
     int amount,
   ) async {
-
     final image = await gameRef.images.load(path);
 
     return SpriteAnimation.fromFrameData(
